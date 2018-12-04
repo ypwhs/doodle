28321e818aae0aaffc4a05fa612497e38c658595
ypwhs <915505626@qq.com>
2018-12-04 22:04:31 +0800

    conv11

--- WideResNeXt.py
+++ WideResNeXt.py
@@ -7,11 +7,19 @@ class BasicBlock(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=4):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes * self.expansion, 3, padding=1, stride=stride, groups=groups)
+        self.conv1 = nn.Conv2d(inplanes, planes * self.expansion,
+                               kernel_size=1, padding=0, stride=stride, groups=1)
         self.bn1 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(planes * self.expansion, planes * self.expansion, 3, padding=1, groups=groups)
+
+        self.conv2 = nn.Conv2d(planes * self.expansion, planes * self.expansion,
+                               kernel_size=3,  padding=1, groups=groups)
         self.bn2 = nn.BatchNorm2d(planes * self.expansion)
+
+        self.conv3 = nn.Conv2d(planes * self.expansion, planes * self.expansion,
+                               kernel_size=3,  padding=1, groups=groups)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+
         self.downsample = downsample
         self.stride = stride
 
@@ -24,6 +32,10 @@ class BasicBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
